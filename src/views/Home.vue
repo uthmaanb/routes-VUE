a2619157c3def53de2f8bd4b7d36ab28e240fc45
ウスマーン <82204694+uthmaanb@users.noreply.github.com>
2021-10-15 09:39:20 +0200

    router links

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+      nostrum ipsam repudiandae magnam tenetur perspiciatis eaque corrupti,
+      harum quae, sunt aliquid. Voluptatem suscipit ut quae nemo aperiam! Optio,
+      maiores aperiam neque adipisci atque similique quam hic delectus ratione
+      porro sint, quo autem commodi fugiat asperiores. Minima quaerat delectus
+      nam nemo harum deleniti, provident quas, itaque sed dolores non, qui
+      voluptatibus placeat nobis explicabo corporis beatae a repellendus atque
+      odit ut ipsum consectetur perspiciatis quod. Unde facilis ipsa atque
+      dolorem aliquam. Animi, facere quia dignissimos, impedit cumque iusto
+      excepturi suscipit aspernatur esse debitis cum nisi, iure labore ut
+      exercitationem dolor placeat!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit modi vero
+      cum distinctio, molestiae ipsum velit libero esse sint quia aliquam
+      ratione, cumque ab iure nulla molestias quo excepturi aperiam, quis
+      deserunt qui inventore repellendus animi. Recusandae, veniam animi facere
+      delectus laborum neque! Modi tempore doloremque, a velit laborum molestias
+      at mollitia est quod magni magnam voluptatibus eos culpa ullam officia
+      dolores atque officiis, voluptate saepe asperiores aliquam suscipit vitae
+      esse. Suscipit modi voluptates, esse porro omnis, impedit ducimus ex ullam
+      necessitatibus, nostrum officia. Molestias, voluptas doloribus minus
+      consectetur dolore necessitatibus ea nesciunt esse sequi provident
+      asperiores quod itaque excepturi?
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
