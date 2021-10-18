6d9a3a713caf7b7869fd2a515022a03ff73f8f79
ウスマーン <82204694+uthmaanb@users.noreply.github.com>
2021-10-18 10:28:31 +0200

    fectching data

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,16 +1,23 @@
 <template>
-  <h1>job details page</h1>
-  <p>The job id is {{ $route.params.id }}</p>
+  <h1>{{ job.title }}</h1>
+  <p>The job id is {{ id }}</p>
+  <p>{{ job.details }}</p>
 </template>
 
 <script>
 export default {
   props: ["id"],
-  // data() {
-  //   return {
-  //     id: this.params.id,
-  //   };
-  // },
+  data() {
+    return {
+      job: null,
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err.message));
+  },
 };
 </script>
 
